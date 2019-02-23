c71eee0de66af7a60fb58283e49c6e18786719a2
Ryan1729 <Ryan1729@gmail.com>
2019-02-23 11:40:48 -0700

    pull `support_gfx` into a separate crate

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use app::update_and_render;
-mod support_gfx;
+use app;
+use imgui_support_gfx;
 
 const CLEAR_COLOR: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
 
 fn main() {
-    support_gfx::run("Some imgui-rs thing".to_owned(), CLEAR_COLOR, update_and_render);
+    imgui_support_gfx::run(
+        "Some imgui-rs thing".to_owned(),
+        CLEAR_COLOR,
+        app::update_and_render,
+    );
 }
