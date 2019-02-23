4387e3d9da2a626dba52e2fb37bbb118dbcc61c0
Ryan1729 <Ryan1729@gmail.com>
2019-02-23 12:55:24 -0700

    how about not blindingly bright?

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use app;
 use imgui_support_gfx;
 
-const CLEAR_COLOR: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+const CLEAR_COLOR: [f32; 4] = [0.0625, 0.0625, 0.0625, 1.0];
 
 fn main() {
     imgui_support_gfx::run(
